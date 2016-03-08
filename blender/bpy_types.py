5197ce23f0c4c06ca8c071502a1ef49b44acb3b6
Joe Eagar <joeedh@gmail.com>
2016-03-08 10:33:29 -0800

    update package.json

---
+++ blender/bpy_types.py
@@ -0,0 +1,12 @@
+class Node:
+  def __init__(self):
+    self.inputs = []
+    self.outputs = []
+  
+class Trigger:
+  def __init__(self):
+    pass
+
+class Region:
+  def __init__(self, polygon):
+    self.polygon = polygon
